ad9076522965b482aaacf3a8ad79f234ced993da
QuiD <wodnd101@naver.com>
2022-06-22 13:22:28 +0900

    init project
    프로젝트 초기설정

---
+++ mileage_BE/src/main/java/mileage/mileage_be/MileageBeApplication.java
@@ -0,0 +1,13 @@
+package mileage.mileage_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileageBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MileageBeApplication.class, args);
+    }
+
+}
---
+++ mileage_BE/src/test/java/mileage/mileage_be/MileageBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mileage.mileage_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileageBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
